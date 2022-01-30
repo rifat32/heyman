9380504b35fd3db2e1930dc78dc0b9f53516308e
rifat32 <rifatbilalphilips@gmail.com>
2022-01-31 04:38:00 +0600

    first commit

--- src/pages/api/topAlbums.ts
+++ src/pages/api/topAlbums.ts
@@ -2,10 +2,14 @@ import { NextApiRequest, NextApiResponse } from 'next'
 const { LASTFM_API_KEY } = process.env
 
 
+
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(
-    `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=venoras&api_key=${LASTFM_API_KEY}&format=json`
+    `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=Kherici&api_key=${LASTFM_API_KEY}&format=json`
   )
   const data = await response.json()
+ 
+
   res.status(200).json(data)
 }
