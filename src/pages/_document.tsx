b806dd118c514d8c3ed7cae35fbcb480d5743618
Kherici <Lionmontana3@gmail.com>
2022-01-31 23:34:59 +0600

    host

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-// import {link} from "../data/link"
-
+import {link} from "../data/link"
+const eren1 = (link +"/eren-1.png");
+const sitemenifest = (link +"/eren-1.png");
 
 class AppDocument extends Document {
   render() {
-   
+   console.log(eren1)
     return (
       <Html lang="en">
         <Head>
@@ -17,12 +18,12 @@ class AppDocument extends Document {
           {/* <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
-           <link rel="apple-touch-icon" sizes="152x152" href={"/eren-1.png"} />
-          <link rel="icon" type="image/png" sizes="32x32" href={"/eren-1.png"} />
-          <link rel="icon" type="image/png" sizes="16x16" href={"/eren-1.png"} />
+           <link rel="apple-touch-icon" sizes="152x152" href={eren1} />
+          <link rel="icon" type="image/png" sizes="32x32" href={eren1} />
+          <link rel="icon" type="image/png" sizes="16x16" href={eren1} />
        
-          <link rel="manifest" href={"/site.webmanifest"} />
-          <link rel="mask-icon" href={"/safari-pinned-tab.svg"} color="#FFFFFF" />
+          <link rel="manifest" href={link +"/site.webmanifest"} />
+          <link rel="mask-icon" href={link +"/safari-pinned-tab.svg"} color="#FFFFFF" />
           <meta property="og:url" content="https://ven.earth" />
           <meta property="og:site_name" content="Erenci" />
           {/* <meta property="og:image" content="/img/ven.jpg" /> */}
