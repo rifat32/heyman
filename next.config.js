910c021e4e5dd735a1892ff069dbe50799dfe77e
Kherici <Lionmontana3@gmail.com>
2022-01-31 23:14:20 +0600

    host

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
     NEXT_PUBLIC_LASTFM_API_KEY: 'ebe2c441c37e356caefe318275757b53',
+    NEXT_PUBLIC_URL: "/Erenci"  
   },
   assetPrefix: '/Erenci',
   basePath:"/Erenci",
