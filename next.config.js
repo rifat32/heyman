d4575b58110d3fcbbf2fd6a7109e1fb0029056c4
Kherici <Lionmontana3@gmail.com>
2022-02-01 23:11:31 +0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   env: {
     // @@@@@@@@@ this is only needed for github pages
-    //  NEXT_PUBLIC_MAIN_APP: 'http://rifat32.github.io/hello',
+      NEXT_PUBLIC_MAIN_APP: 'https://rifat32.github.io/heyman',
     // @@@@@@@@@
     NEXT_PUBLIC_LASTFM_API_KEY: 'ebe2c441c37e356caefe318275757b53',
     NEXT_PUBLIC_Weather_API_KEY: 'e3b952b7c233305e18697b3645934aa6',
     
   },
   // @@@@@@@@@ this is only needed for github pages 
-  //  assetPrefix: '/hello',
-  //  basePath:"/hello",
+    assetPrefix: '/heyman',
+    basePath:"/heyman",
   // @@@@@@@@@
   
  
