5be19cc3b36cfeec32d2b4fc5025c6af62aa6d83
Kherici <Lionmontana3@gmail.com>
2022-02-01 20:36:01 +0600

    first commit

--- src/data/link.ts
+++ src/data/link.ts
@@ -1,9 +1,9 @@
 export const link = {
-    eren1png:"https://kherici.github.io/Erenci/eren-1.png",
-    safaripinnedtabsvg:"https://kherici.github.io/Erenci/safari-pinned-tab.svg",
-    webmanifest:"https://kherici.github.io/Erenci/site.webmanifest",
+    eren1png:"https://kherici.github.io/Kherici/eren-1.png",
+    safaripinnedtabsvg:"https://kherici.github.io/Kherici/safari-pinned-tab.svg",
+    webmanifest:"https://kherici.github.io/Kherici/site.webmanifest",
     
-    link:"https://kherici.github.io/Erenci"
+    link:"https://kherici.github.io/Kherici"
     
     
 }
